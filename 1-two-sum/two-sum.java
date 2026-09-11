3ab2ac11bea5864127635c5ddc93216d9e5e031f
Rohit Kumar Mahato <156408369+ro-hitkr@users.noreply.github.com>
2026-09-11 23:23:19 +0530

    Time: 4 ms (52.00%) | Memory: 46.4 MB (98.77%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,34 +1,21 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-    int[][] numWithIndex = new int[nums.length][2];
-        for (int i = 0; i < nums.length; i++) {
-            numWithIndex[i][0] = nums[i]; // The actual number
-            numWithIndex[i][1] = i;       // The original index
-        }
-        
-        // Step 2: Sort the 2D array based on the numbers (column 0)
-        Arrays.sort(numWithIndex, Comparator.comparingInt(a -> a[0]));
-        
-        // Step 3: Initialize two pointers
-        int left = 0;
-        int right = nums.length - 1;
-        
-        // Step 4: Two-pointer search loop
-        while (left < right) {
-            int currentSum = numWithIndex[left][0] + numWithIndex[right][0];
-            
-            if (currentSum == target) {
-                // Return the stored original indices from column 1
-                return new int[]{numWithIndex[left][1], numWithIndex[right][1]};
+        Map<Integer, Integer> numMap = new HashMap<>();
+        int n = nums.length;
 
+        // Build the hash table
+        for (int i = 0; i < n; i++) {
+            numMap.put(nums[i], i);
         }
-        else if(currentSum<target){
-            left++;
+
+        // Find the complement
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement) && numMap.get(complement) != i) {
+                return new int[]{i, numMap.get(complement)};
+            }
         }
-        else{
-            right--;
-        }
-    }       
-    return new int[]{};
+
+        return new int[]{}; // No solution found
     }
 }
